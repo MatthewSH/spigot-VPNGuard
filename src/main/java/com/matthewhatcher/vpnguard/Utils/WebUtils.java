cf06aff87b413c171cc4fb99c614d1be25241b68
Matthew H <hello@matthewh.in>
2016-09-12 10:10:25 -0400

    Adding webutils.

---
+++ src/main/java/com/matthewhatcher/vpnguard/Utils/WebUtils.java
@@ -0,0 +1,12 @@
+package com.matthewhatcher.vpnguard.Utils;
+
+import com.matthewhatcher.vpnguard.VPNGuard;
+
+public class WebUtils 
+{
+	private VPNGuard plugin;
+	
+	public WebUtils(VPNGuard plugin) {
+		this.plugin = plugin;
+	}
+}
