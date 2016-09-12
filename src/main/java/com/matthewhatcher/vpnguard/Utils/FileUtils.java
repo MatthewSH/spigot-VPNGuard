e0f827f36c65a5a79d508cfd6ba7c3c7fbe0a4b9
Matthew H <hello@matthewh.in>
2016-09-12 10:10:15 -0400

    Adding file utils.

--- src/main/java/com/matthewhatcher/vpnguard/Utils/FileUtils.java
+++ src/main/java/com/matthewhatcher/vpnguard/Utils/FileUtils.java
@@ -1,6 +1,13 @@
 package com.matthewhatcher.vpnguard.Utils;
 
+import com.matthewhatcher.vpnguard.VPNGuard;
+
 public class FileUtils 
 {
+	private VPNGuard plugin;
+	
+	public FileUtils(VPNGuard plugin) {
+		this.plugin = plugin;
+	}
 
 }
