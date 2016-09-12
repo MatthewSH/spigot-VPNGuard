4e6f60a28bb1ff4a39946aee0b3fb7804af8bd43
Matthew H <hello@matthewh.in>
2016-09-12 09:09:01 -0400

    Adding plugin overrides and event listener registration.

--- src/main/java/com/matthewhatcher/vpnguard/VPNGuard.java
+++ src/main/java/com/matthewhatcher/vpnguard/VPNGuard.java
@@ -1,6 +1,28 @@
 package com.matthewhatcher.vpnguard;
 
-public class VPNGuard 
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.matthewhatcher.vpnguard.Listeners.LoginListener;
+
+public class VPNGuard extends JavaPlugin
 {
+	private VPNGuard instance;
+	public LoginListener loginListener;
 	
+	@Override
+	public void onEnable() {
+		this.instance = this;
+		loginListener = new LoginListener(this.instance);
+		
+		this.getServer().getPluginManager().registerEvents(loginListener, this.instance);
+		
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
 }
