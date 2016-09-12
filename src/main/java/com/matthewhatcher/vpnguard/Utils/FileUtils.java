40793e54ca9b2123a2c98ef7eee072d99abecd1e
Matthew H <hello@matthewh.in>
2016-09-12 10:19:53 -0400

    Adding cached IPs list.

--- src/main/java/com/matthewhatcher/vpnguard/Utils/FileUtils.java
+++ src/main/java/com/matthewhatcher/vpnguard/Utils/FileUtils.java
@@ -1,10 +1,14 @@
 package com.matthewhatcher.vpnguard.Utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.matthewhatcher.vpnguard.VPNGuard;
 
 public class FileUtils 
 {
 	private VPNGuard plugin;
+	public List<String> cachedIPs = new ArrayList<String>();
 	
 	public FileUtils(VPNGuard plugin) {
 		this.plugin = plugin;
