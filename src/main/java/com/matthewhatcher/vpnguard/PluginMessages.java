7fb4520d4c584f3e6fdcab9416bef4de20f5e843
Matthew H <hello@matthewh.in>
2016-09-12 09:54:43 -0400

    Adding messages! :notebook:
    We all need messages. So here they are!

---
+++ src/main/java/com/matthewhatcher/vpnguard/PluginMessages.java
@@ -0,0 +1,25 @@
+package com.matthewhatcher.vpnguard;
+
+import org.bukkit.ChatColor;
+
+public class PluginMessages 
+{
+	public static String DEFAULT_KICK_MESSAGE = "Sorry, but your IP (%ip) seems to be a VPN and you were kicked. Please turn it off and try again.";
+	public static String PREFIX = ChatColor.WHITE + "[" + 
+								  ChatColor.GOLD + "VPN Guard" +
+								  ChatColor.WHITE + "] ";
+	public static String PREFIX_NOCOLOR = "[VPN Guard] ";
+	public static String NO_PERMISSION = PREFIX + "Sorry, but you do not have permission to run that command.";
+	public static String INVALID_COMMAND = PREFIX + "I don't recognize that command.";
+	public static String[] HELP_MESSAGE_COMMANDS = {
+		"/vpng refresh | Force refreshes the cache.",
+		"/vpng reload  | Reloads the plugin configuration"
+	};
+	public static String COMMAND_REFRESH = PREFIX + "Refreshing cache...";
+	public static String COMMAND_REFRESH_COMPLETED = PREFIX + "Cache has been refreshed.";
+	public static String COMMAND_RELOAD = PREFIX + "Reloading config...";
+	public static String COMMAND_RELOAD_COMPLETED = PREFIX + "Configuration has been reloaded.";
+	public static String CONSOLE_BLOCKEDLOGIN = PREFIX_NOCOLOR + "";
+	public static String CONSOLE_COMMAND_PURGE = PREFIX_NOCOLOR + "Okay then, clearing the cache file.";
+	public static String CONSOLE_COMMAND_PURGE_COMPLETED = PREFIX_NOCOLOR + "Cache file has been emptied, may God help you.";
+}
