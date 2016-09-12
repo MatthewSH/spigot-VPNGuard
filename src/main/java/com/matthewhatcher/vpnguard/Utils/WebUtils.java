725f172354766b04f65c01cc44e0c1930f74f437
Matthew H <hello@matthewh.in>
2016-09-12 10:20:05 -0400

    Adding web utils constructor.

--- src/main/java/com/matthewhatcher/vpnguard/Utils/WebUtils.java
+++ src/main/java/com/matthewhatcher/vpnguard/Utils/WebUtils.java
@@ -6,7 +6,26 @@ public class WebUtils
 {
 	private VPNGuard plugin;
 	
+	String apiUrl;
+	String fallbackApiUrl;
+	
 	public WebUtils(VPNGuard plugin) {
 		this.plugin = plugin;
+		
+		if(plugin.config.hasEmail()) {
+			apiUrl = "http://check.getipintel.net/check.php?ip=%ip%&contact=" + plugin.config.adminEmail;
+			fallbackApiUrl = "http://legacy.iphub.info/api.php?ip=%ip%&showtype=4&email=" + plugin.config.adminEmail;
+		} else {
+			apiUrl = "http://check.getipintel.net/check.php?ip=%ip%";
+			fallbackApiUrl = "http://legacy.iphub.info/api.php?ip=%ip%&showtype=4";
+		}
+	}
+	
+	public boolean apiIsOnline() {
+		return true;
+	}
+	
+	public boolean fallbackIsOnline() {
+		return true;
 	}
 }
