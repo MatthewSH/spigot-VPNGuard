6ff4be17c347c91e680af72bb1d46a898617da82
Matthew H <hello@matthewh.in>
2016-09-12 10:10:38 -0400

    Adding file and config variables.

--- src/main/java/com/matthewhatcher/vpnguard/VPNGuard.java
+++ src/main/java/com/matthewhatcher/vpnguard/VPNGuard.java
@@ -3,16 +3,26 @@ package com.matthewhatcher.vpnguard;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.matthewhatcher.vpnguard.Listeners.LoginListener;
+import com.matthewhatcher.vpnguard.Utils.ConfigUtils;
+import com.matthewhatcher.vpnguard.Utils.FileUtils;
 
 public class VPNGuard extends JavaPlugin
 {
 	private VPNGuard instance;
 	public LoginListener loginListener;
+	public ConfigUtils config;
+	public FileUtils file;
+	
+	public VPNGuard getInstance() {
+		return this.instance;
+	}
 	
 	@Override
 	public void onEnable() {
 		this.instance = this;
-		loginListener = new LoginListener(this.instance);
+		loginListener = new LoginListener(getInstance());
+		config = new ConfigUtils(getInstance());
+		file = new FileUtils(getInstance());
 		
 		this.getServer().getPluginManager().registerEvents(loginListener, this.instance);
 		
