468a21352394d53dcde620436fb70248a458ac21
Matthew H <hello@matthewh.in>
2016-09-12 10:10:03 -0400

    Adding config stuff.

--- src/main/java/com/matthewhatcher/vpnguard/Utils/ConfigUtils.java
+++ src/main/java/com/matthewhatcher/vpnguard/Utils/ConfigUtils.java
@@ -1,16 +1,37 @@
 package com.matthewhatcher.vpnguard.Utils;
 
+import com.matthewhatcher.vpnguard.PluginMessages;
 import com.matthewhatcher.vpnguard.VPNGuard;
 
 public class ConfigUtils 
 {
 	private VPNGuard plugin;
 	
+	public boolean allowVPNs = true;
+	public String kickMessage = PluginMessages.DEFAULT_KICK_MESSAGE;
+	public String adminEmail = null;
+	
 	public ConfigUtils(VPNGuard plugin) {
 		this.plugin = plugin;
 	}
 	
-	public void saveDefaultConfig() {
+	public void loadConfig() {
+		if(plugin.getConfig().getString("email-address") != null) {
+			this.adminEmail = plugin.getConfig().getString("email-address");
+		}
 		
+		this.kickMessage = plugin.getConfig().getString("kick-message");
+		this.allowVPNs = plugin.getConfig().getBoolean("allow-vpn-connection");
+	}
+	
+	public void saveDefaultConfig() {
+		plugin.getConfig().addDefault("kick-message", PluginMessages.DEFAULT_KICK_MESSAGE);
+		plugin.getConfig().addDefault("allow-vpn-connection", true);
+		plugin.getConfig().options().copyDefaults(true);
+		plugin.saveConfig();
+	}
+	
+	public boolean hasEmail() {
+		return (adminEmail != null);
 	}
 }
