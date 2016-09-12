0ac5415e34238a9f6aa315eb9c7c3df00950c5e8
Matthew H <hello@matthewh.in>
2016-09-12 09:46:09 -0400

    Adding plugin reference.

--- src/main/java/com/matthewhatcher/vpnguard/Utils/ConfigUtils.java
+++ src/main/java/com/matthewhatcher/vpnguard/Utils/ConfigUtils.java
@@ -1,6 +1,16 @@
 package com.matthewhatcher.vpnguard.Utils;
 
+import com.matthewhatcher.vpnguard.VPNGuard;
+
 public class ConfigUtils 
 {
-
+	private VPNGuard plugin;
+	
+	public ConfigUtils(VPNGuard plugin) {
+		this.plugin = plugin;
+	}
+	
+	public void saveDefaultConfig() {
+		
+	}
 }
