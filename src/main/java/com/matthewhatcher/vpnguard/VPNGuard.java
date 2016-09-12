794649c35ec080c90717260082129cd0a36e32fa
Matthew H <hello@matthewh.in>
2016-09-12 10:20:18 -0400

    Initializing the web variable.

--- src/main/java/com/matthewhatcher/vpnguard/VPNGuard.java
+++ src/main/java/com/matthewhatcher/vpnguard/VPNGuard.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.matthewhatcher.vpnguard.Listeners.LoginListener;
 import com.matthewhatcher.vpnguard.Utils.ConfigUtils;
 import com.matthewhatcher.vpnguard.Utils.FileUtils;
+import com.matthewhatcher.vpnguard.Utils.WebUtils;
 
 public class VPNGuard extends JavaPlugin
 {
@@ -12,6 +13,8 @@ public class VPNGuard extends JavaPlugin
 	public LoginListener loginListener;
 	public ConfigUtils config;
 	public FileUtils file;
+	public WebUtils web;
+	
 	
 	public VPNGuard getInstance() {
 		return this.instance;
@@ -23,6 +26,7 @@ public class VPNGuard extends JavaPlugin
 		loginListener = new LoginListener(getInstance());
 		config = new ConfigUtils(getInstance());
 		file = new FileUtils(getInstance());
+		web = new WebUtils(getInstance());
 		
 		this.getServer().getPluginManager().registerEvents(loginListener, this.instance);
 		
