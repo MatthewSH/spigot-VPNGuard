8aa2d61cbc3f0431de5b7442c2d0a981b371351f
Matthew H <hello@matthewh.in>
2016-09-12 23:46:49 -0400

    Adding command executors.

--- src/main/java/com/matthewhatcher/vpnguard/VPNGuard.java
+++ src/main/java/com/matthewhatcher/vpnguard/VPNGuard.java
@@ -2,6 +2,8 @@ package com.matthewhatcher.vpnguard;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.matthewhatcher.vpnguard.Commands.AdminCommands;
+import com.matthewhatcher.vpnguard.Commands.ConsoleCommands;
 import com.matthewhatcher.vpnguard.Listeners.LoginListener;
 import com.matthewhatcher.vpnguard.Utils.ConfigUtils;
 import com.matthewhatcher.vpnguard.Utils.FileUtils;
@@ -29,7 +31,9 @@ public class VPNGuard extends JavaPlugin
 		file = new FileUtils(getInstance());
 		
 		this.getServer().getPluginManager().registerEvents(loginListener, this.instance);
-		
+		this.getCommand("vpng").setExecutor(new AdminCommands(this));
+		this.getCommand("vpnguard").setExecutor(new AdminCommands(this));
+		this.getCommand("vpngpurge").setExecutor(new ConsoleCommands(this));
 		super.onEnable();
 	}
 
