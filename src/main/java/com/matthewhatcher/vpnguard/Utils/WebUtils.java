a06e9412cd91fbeac5a458d4e9d1ccfdc5fd9027
Matthew H <hello@matthewh.in>
2016-09-12 22:48:19 -0400

    Massive update to web utilities.

--- src/main/java/com/matthewhatcher/vpnguard/Utils/WebUtils.java
+++ src/main/java/com/matthewhatcher/vpnguard/Utils/WebUtils.java
@@ -1,13 +1,18 @@
 package com.matthewhatcher.vpnguard.Utils;
 
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.matthewhatcher.vpnguard.VPNGuard;
 
 public class WebUtils 
 {
+	@SuppressWarnings("unused")
 	private VPNGuard plugin;
-	
 	String apiUrl;
 	String fallbackApiUrl;
+	String mainUrl = "getipintel.net";
+	String fallbackUrl = "iphub.info";
 	
 	public WebUtils(VPNGuard plugin) {
 		this.plugin = plugin;
@@ -22,14 +27,56 @@ public class WebUtils
 	}
 	
 	public boolean apiIsOnline() {
-		return true;
+		if(apiUrl == null)
+			return false;
+		
+		if(online(mainUrl))
+			return true;
+		else
+			return false;
 	}
 	
 	public boolean fallbackIsOnline() {
-		return true;
+		if(fallbackApiUrl == null)
+			return false;
+		
+		if(online(fallbackUrl))
+			return true;
+		else
+			return false;
 	}
 	
 	public boolean isVPN(String ip) {
-		return true;
+		JsonObject obj;
+		String response;
+		
+		if((apiUrl != null && apiUrl.length() > 2) && apiIsOnline()) {
+			response = HttpRequest.get(apiUrl.replace("%ip%", ip)).body();
+			obj = new JsonParser().parse(response).getAsJsonObject();
+			
+			if(obj.get("status").getAsString().equalsIgnoreCase("success")) {
+				if(obj.get("result").getAsInt() == 1)
+					return true;
+			}
+				
+			return false;
+		} else if((fallbackApiUrl != null && fallbackApiUrl.length() > 2) && fallbackIsOnline()) {
+			response = HttpRequest.get(fallbackApiUrl.replace("%ip%", ip)).body();
+			obj = new JsonParser().parse(response).getAsJsonObject();
+			if(obj.get("proxy").getAsInt() == 1)
+				return true;
+				
+			return false;
+		}
+		
+		return false;
+	}
+	
+	private boolean online(String host) {
+		String response = HttpRequest.get(host).body();
+		if(response.contains("It's just you")) 
+			return true;
+		else
+			return false;
 	}
 }
